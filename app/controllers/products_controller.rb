b806f17a13faf75f0fab10777ff28fe1517cf862
david-lake <lakedavid23@gmail.com>
2022-02-11 12:45:41 +0000

    Add API pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,7 @@ class ProductsController < ApplicationController
           @title    = "Shop all"
         end
       }
-      format.json { render json: Product.all }
+      format.json { render json: Product.paginate(page: params[:page], per_page: params[:per_page]) }
     end
   end
 

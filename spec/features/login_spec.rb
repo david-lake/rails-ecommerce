c79cafd86f631993c9bc5ca8d911435f229c3888
david-lake <lakedavid23@gmail.com>
2022-03-11 20:13:24 +0000

    Add unsuccesfull login feature test

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -4,13 +4,21 @@ feature 'Login' do
 
   before do
     @user = FactoryBot.create(:user)
+    visit login_path
   end
 
   scenario 'successfull' do
-    visit login_path
     fill_in "Email", with: @user.email
     fill_in "Password", with: @user.password
     click_button "Login"
     expect(page).to have_css "#account.dropdown"
+  end
+
+  scenario 'unsuccessfull' do
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: "incorrect"
+    click_button "Login"
+    expect(page).to have_no_css "#account.dropdown"
+    expect(find(".alert-danger").text).to eq "Invalid email or password"
   end
 end
